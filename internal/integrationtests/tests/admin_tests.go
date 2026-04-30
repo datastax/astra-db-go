702d8503ef2061c96ee83f23f47b96bd529e5761
Dean Davidson <deand@sembit.com>
2026-04-30 09:07:11 -0700

    Integration tests:
    - Change region to something that supports free tier
    - Set defaults / document behavior in TestEnv

--- internal/integrationtests/tests/admin_tests.go
+++ internal/integrationtests/tests/admin_tests.go
@@ -129,8 +129,8 @@ func AdminCreateDropDatabase(e *harness.TestEnv) error {
 	// Create a database (non-blocking to keep test fast)
 	params := astradb.CreateDatabaseParams{
 		Name:          "go-sdk-integration-test",
-		CloudProvider: "gcp",
-		Region:        "us-east1",
+		CloudProvider: "aws",
+		Region:        "us-east-2",
 	}
 
 	slog.Info("Creating database", "name", params.Name, "provider", params.CloudProvider, "region", params.Region)
