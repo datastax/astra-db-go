7165c7627316354a87c91535d5f5a8bdc1835196
toptobes <kavinpg@gmail.com>
2026-06-09 20:13:53 -0500

    a

--- tools/gen-options/main.go
+++ tools/gen-options/main.go
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
+
 // gen-options generates boilerplate for the options package:
 //
 //  1. Children() methods — for each struct with *Validator fields, emits a
