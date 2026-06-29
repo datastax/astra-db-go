40e647c4b361906ada9b1758e88575d13746fe07
toptobes <kavinpg@gmail.com>
2026-06-29 17:03:06 -0500

    bump version

--- astra/internal/constants/version.go
+++ astra/internal/constants/version.go
@@ -15,7 +15,7 @@
 package constants
 
 // LibVersion is the version of the library.
-const LibVersion = "2.0.0-preview.3"
+const LibVersion = "2.0.0-preview.4"
 
 // LibName is the name of the library.
 const LibName = "astra-db-go"
